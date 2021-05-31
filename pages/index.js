9605106093554e4b33f470a57376d5ca4c8e798e
MamunAmin <mamun.amin2020@gmail.com>
2021-05-31 21:10:03 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Products } from '../components';
 import styles from '../styles/Home.module.css';
-import { server, dev} from '../config';
+import { server } from '../config';
 
 export default function Home({ products }) {
   return (
@@ -11,15 +11,8 @@ export default function Home({ products }) {
 }
 
 export const getServerSideProps = async (context) => {
-  if(!dev) {
-    let fs = require('fs');
-    let data = fs.readFileSync("../data/products.json", 'utf8');
-    var Products = JSON.parse(data)
-  }
-  else {
     const res = await fetch(`${server}/api/products`)
-    var Products = await res.json()
-  }
+    const Products = await res.json()
 
   return {
     props: {
